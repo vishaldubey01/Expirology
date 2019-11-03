ab68206ab41323db66bf5a5c7a7d928296fda1e9
Vishal Dubey <vdubey01@gmail.com>
2019-11-03 05:14:35 -0500

    done?

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 #!flask/bin/python
-from flask import Flask
-from flask import jsonify
-from flask import abort
-from flask import make_response
-from flask import request
-from flask import url_for
-from numpy.core import unicode
+
+
+import time
 import data_processing
+import os
+from flask import Flask
+from flask import request
+from flask import abort
+from flask import redirect
+from flask import url_for
+from flask import make_response
+from flask import send_from_directory
+from flask import jsonify
+from werkzeug.utils import secure_filename
+from werkzeug.urls import URL
+
 
 app = Flask(__name__)
 
@@ -18,6 +26,10 @@ ret = {
 error = {
     'error': 'Not a food'
 }
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Custom API for handling OCR and backend"
 
 @app.route('/foods', methods=['GET'])
 def get_tmp():
@@ -31,6 +43,47 @@ def get_date(foodName):
     if(rets=="ERROR"):
         return error
     return jsonify(rets)
+
+UPLOAD_FOLDER = './files'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+picsList=[]
+
+###################
+#  Download zone  #
+###################
+
+@app.route('/picture', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        return "hi"
+        filename = str(time.time()).replace(".", "") + ".jpg"
+
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'upload':True, 'name' : filename})
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+@app.route('/get/picture/<string:name>', methods=['GET'])
+def send_pics(name):
+    pics = open("./files/" + name)
+    if pics:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], name)
+
+    abort(404)
+
+
+@app.errorhandler(400)
+def not_complete(error):
+    return make_response(jsonify({'error' : 'request not complete'}), 400)
 
 @app.errorhandler(404)
 def not_found(error):
